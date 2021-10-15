616647f8b104e131d33ff66130a87e203222ae78
Marc Bakker <webdev@marcbakker.com>
2021-10-15 20:56:34 +0200

    Add logo component + showroom page.

--- projects/ui-components/src/lib/components/logo/logo.component.ts
+++ projects/ui-components/src/lib/components/logo/logo.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
 @Component({
-  selector: 'lib-logo',
-  templateUrl: './logo.component.html',
-  styleUrls: ['./logo.component.css']
+	selector: 'lib-logo',
+	templateUrl: './logo.component.html',
+	styleUrls: ['./logo.component.scss'],
 })
 export class LogoComponent implements OnInit {
+	@Input() public imageUrl!: string;
+	@Input() public alt: string = '';
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
