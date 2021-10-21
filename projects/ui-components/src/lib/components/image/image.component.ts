693fcb064d323a08b46b715dfae561c80d8207fe
Marc Bakker <webdev@marcbakker.com>
2021-10-21 13:13:26 +0200

    Add Image component and showroom page with examples.

--- projects/ui-components/src/lib/components/image/image.component.ts
+++ projects/ui-components/src/lib/components/image/image.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'lib-image',
-  templateUrl: './image.component.html',
-  styleUrls: ['./image.component.scss']
+	selector: 'lib-image[imageUrl]',
+	templateUrl: './image.component.html',
+	styleUrls: ['./image.component.scss'],
 })
-export class ImageComponent implements OnInit {
+export class ImageComponent {
+	@Input() imageUrl!: string;
+	@Input() alt: string = '';
+	@Input() caption: string = '';
+	@Input() expand: boolean = false;
 
-  constructor() { }
+	public expanded: boolean = false;
+	public containerClass: string = 'container';
+	public imageClass: string = 'image__thumbnail';
 
-  ngOnInit(): void {
-  }
+	ngOnInit() {
+		this.imageClass += this.expand ? ' image--cursorPointer' : '';
+	}
 
+	expandImage() {
+		if (this.expand) {
+			this.expanded = !this.expanded;
+			this.containerClass = this.expanded ? 'container__expanded' : 'container';
+			this.imageClass = this.expanded ? 'image__expanded' : 'image__thumbnail';
+			this.imageClass += ' image--cursorPointer';
+		}
+	}
 }
