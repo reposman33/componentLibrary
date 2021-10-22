92684a1dc994d29f00af72bbe16567ecbeea1792
Marc Bakker <webdev@marcbakker.com>
2021-10-22 22:10:34 +0200

    Image component: make image expandable without messing up containing element.

--- projects/ui-components/src/lib/components/image/image.component.ts
+++ projects/ui-components/src/lib/components/image/image.component.ts
@@ -9,20 +9,19 @@ export class ImageComponent {
 	@Input() imageUrl!: string;
 	@Input() alt: string = '';
 	@Input() caption: string = '';
-	@Input() expand: boolean = false;
+	@Input() expandable: boolean = false;
 
 	public expanded: boolean = false;
 	public containerClass: string = 'container';
 	public imageClass: string = 'image__thumbnail';
 
 	ngOnInit() {
-		this.imageClass += this.expand ? ' image--cursorPointer' : '';
+		this.imageClass += this.expandable ? ' image--cursorPointer' : '';
 	}
 
 	expandImage() {
-		if (this.expand) {
+		if (this.expandable) {
 			this.expanded = !this.expanded;
-			this.containerClass = this.expanded ? 'container__expanded' : 'container';
 			this.imageClass = this.expanded ? 'image__expanded' : 'image__thumbnail';
 			this.imageClass += ' image--cursorPointer';
 		}
